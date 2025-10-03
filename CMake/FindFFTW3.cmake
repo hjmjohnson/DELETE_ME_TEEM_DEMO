5f8b2f1e2364ccab920765fbd40d9231a8e5e3a6
Hans Johnson <hans-johnson@uiowa.edu>
2025-10-03 13:08:51 -0500

    chore: Update to modern CMake best practices.

--- CMake/FindFFTW3.cmake
+++ CMake/FindFFTW3.cmake
@@ -1,22 +1,151 @@
+## FindFFTW3.cmake: Slightly better way of looking for FFTW3 package
+#    ("fftw3" = http://www.fftw.org)
+# Copyright (C) 2025  University of Chicago
+# See ../LICENSE.txt for licensing terms
 
-find_path(FFTW3_INCLUDE_DIR fftw3.h
-  /usr/local/include
-  /usr/include
-)
+# Even though CMake already has a built-in FindFFTW3.cmake, it assumes that fftw3 was
+# installed locally via CMake, but that's not what e.g. (mac) brew or (linux) apt
+# actually does, so find_package(FFTW3) can succeed but is not able to define imported
+# targets for use with target_link_libraries(FFTW3::fftw3) This is a long-standing issue:
+# https://github.com/FFTW/fftw3/issues/130
+#
+# This module defines:
+#   FFTW3_FOUND        - True if both header and library were found
+#   FFTW3_INCLUDE_DIR  - Directory containing fftw3.h
+#                        (legacy; for include_directories)
+#   FFTW3_LIBRARY      - Full path to the fftw3 library file
+#                        (legacy; for direct linking)
+#   FFTW3_INCLUDE_DIRS - Directories to add to include path
+#                        (legacy; currently just fftw3)
+#   FFTW3_LIBRARIES    - Libraries to link
+#                        (legacy; just fftw3, no transitive deps)
+#   FFTW3_VERSION      - Optional version string for the library; not reliably
+#                        parse-able from fftw3.h, so will be empty unless
+#                        supplied by the user or cache.
+#
+# Imported target (modern CMake usage):
+#   FFTW3::fftw3       - Imported target for modern target_link_libraries usage
+#
+# Usage:
+#   find_package(FFTW3 REQUIRED)
+#   target_link_libraries(myexe PRIVATE FFTW3::fftw3)
+#   # or for legacy code:
+#   target_include_directories(myexe PRIVATE ${FFTW3_INCLUDE_DIRS})
+#   target_link_libraries(myexe PRIVATE ${FFTW3_LIBRARIES})
+#
+# For comparison look at:
+# https://github.com/egpbos/findFFTW
+# https://github.com/acoustid/chromaprint/blob/master/cmake/modules/FindFFTW3.cmake
+# https://git.astron.nl/RD/EveryBeam/-/blob/v0.6.2/CMake/FindFFTW3.cmake
 
-find_library(FFTW3_LIBRARY fftw3
-  /usr/lib
-  /usr/local/lib
-)
+# TODO: handle the various variants for different precisions
+# See FindFFTW3-multiprec.cmake for inspiration
 
-set(FFTW3_FOUND FALSE)
-if(FFTW3_INCLUDE_DIR AND FFTW3_LIBRARY)
-    set(FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
-    set(FFTW3_FOUND TRUE)
+# how we identify ourselves
+set(_dep "FFTW3")
+
+# Option to enable extra debug output from this module
+option(Teem_DEBUG_${_dep} "Print detailed debug about finding ${_dep}" OFF)
+
+# Helper macros to print only when Teem_DEBUG_${_dep} is enabled
+macro(_status msg)
+  if(Teem_DEBUG_${_dep})
+    message(STATUS "[Find${_dep}] ${msg}")
+  endif()
+endmacro()
+macro(_check_start msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_START "[Find${_dep}] ${msg}")
+  endif()
+endmacro()
+macro(_check_fail msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_FAIL "${msg}")
+  endif()
+endmacro()
+macro(_check_pass msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_PASS "${msg}")
+  endif()
+endmacro()
+
+# NOTE: If a working FFTW3Config.cmake is found, it will define
+# FFTW3::fftw3 for us already, so we don’t have to do anything else.
+# This fallback only runs when FFTW3_FOUND is still FALSE.
+if(NOT FFTW3_FOUND)
+
+  # Find the directory containing fftw3.h. Users can help by setting FFTW3_DIR or
+  # by making sure the header is somewhere in the default search paths.
+  find_path(FFTW3_INCLUDE_DIR
+    NAMES fftw3.h
+    HINTS
+      $ENV{FFTW3_DIR}
+      ${CMAKE_INSTALL_PREFIX}
+      /usr
+      /usr/local
+    PATH_SUFFIXES
+      include
+      include/fftw3
+    DOC "Directory containing fftw3.h"
+  )
+  _status("find_path result: FFTW3_INCLUDE_DIR='${FFTW3_INCLUDE_DIR}'")
+
+  # Look for the FFTW3 library. Can set FFTW3_DIR to containing directory.
+  find_library(FFTW3_LIBRARY
+    NAMES fftw3
+    HINTS
+      $ENV{FFTW3_DIR}
+      ${CMAKE_INSTALL_PREFIX}
+      /usr
+      /usr/local
+    PATH_SUFFIXES
+      lib
+      lib64
+    DOC "Full path to the libfftw3.\{a\|so\|dylib\|lib\} file"
+  )
+  _status("find_library result: FFTW3_LIBRARY='${FFTW3_LIBRARY}'")
+
+  # Optional user-provided version (since fftw3.h does not seem to declare its own
+  # version in any way that can be learned at configure- or compile-time (the info is
+  # available at run-time, but that's too annoying).
+  if(NOT DEFINED FFTW3_VERSION)
+    set(FFTW3_VERSION "" CACHE STRING "Optional: version of FFTW3 library")
+  endif()
+
+  # Include the helper macro for standard handling of results
+  include(FindPackageHandleStandardArgs)
+  # Sets FFTW3_FOUND if successful
+  _check_start("find_package_handle_standard_args(FFTW3)")
+  find_package_handle_standard_args(FFTW3
+    REQUIRED_VARS FFTW3_LIBRARY FFTW3_INCLUDE_DIR
+    VERSION_VAR FFTW3_VERSION
+  )
+
+  if (FFTW3_FOUND)
+    # for use by old-style CMake code
+    set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR})
+    set(FFTW3_LIBRARIES ${FFTW3_LIBRARY})
+
+    # If we did find FFTW3, but no imported target exists yet,
+    # create our own IMPORTED target so downstream code can use:
+    #     target_link_libraries(myprog PRIVATE FFTW3::fftw3)
+    if (NOT TARGET FFTW3::fftw3)
+      add_library(FFTW3::fftw3 UNKNOWN IMPORTED)
+      set_target_properties(FFTW3::fftw3 PROPERTIES
+        IMPORTED_LOCATION             "${FFTW3_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES      "${FFTW3_LIBRARY}"  # maybe redundant?
+      )
+    endif()
+    _check_pass("Found:\n      ${_dep}_INCLUDE_DIR=${${_dep}_INCLUDE_DIR}\n      ${_dep}_LIBRARY=${${_dep}_LIBRARY}")
+  else()
+    _check_fail("Not found")
+  endif()
 endif()
 
-mark_as_advanced(
-  FFTW3_INCLUDE_DIR
-  FFTW3_LIBRARIES
-  FFTW3_FOUND
-  )
+# clean up
+unset(_dep)
+unset(_status)
+unset(_check_start)
+unset(_check_fail)
+unset(_check_pass)
--- CMake/FindLEVMAR.cmake
+++ CMake/FindLEVMAR.cmake
@@ -1,24 +1,190 @@
+## FindLEVMAR.cmake: Careful+verbose way of looking for a levmar install
+#    ("levmar" = https://users.ics.forth.gr/~lourakis/levmar/index.html)
+# Copyright (C) 2025  University of Chicago
+# See ../LICENSE.txt for licensing terms
 
-find_path(LEVMAR_INCLUDE_DIR levmar.h
-  /usr/local/include/levmar
-  /usr/local/include
-  /usr/include
+# This module tries to find a Levmar library and header, and sets variables/targets
+# so other CMake projects can use it.
+#
+# Variables defined by this module:
+#   LEVMAR_FOUND        - True if both levmar.h and the levmar library were found
+#   LEVMAR_INCLUDE_DIR  - Directory containing levmar.h
+#                         (legacy; for include_directories)
+#   LEVMAR_LIBRARY      - Full path to the levmar library file
+#                         (legacy; for direct linking)
+#   LEVMAR_INCLUDE_DIRS - List of include directories
+#                         (legacy; typically just LEVMAR_INCLUDE_DIR)
+#   LEVMAR_LIBRARIES    - List of libraries to link
+#                         (legacy; levmar plus optional LAPACK/BLAS)
+#   LEVMAR_VERSION      - Version string parsed from levmar.h (if available)
+#
+# Imported target (modern CMake usage):
+#   LEVMAR::LEVMAR      - Imported target encapsulating include directories,
+#                         library, and optional transitive dependencies
+#                         (LAPACK/BLAS).
+#
+# Usage (modern style):
+#   find_package(LEVMAR REQUIRED)
+#   target_link_libraries(myexe PRIVATE LEVMAR::LEVMAR)
+#
+# Usage (legacy style, for code expecting variables):
+#   find_package(LEVMAR REQUIRED)
+#   target_link_libraries(myexe PRIVATE ${LEVMAR_LIBRARIES})
+#   include_directories(${LEVMAR_INCLUDE_DIRS})
+#
+# Notes:
+# - This module attempts to find LAPACK and BLAS libraries if a levmar library is found.
+#   These are included in the imported target and ${LEVMAR_LIBRARIES} if available.
+# - Versions prior to 2.5 did not provide levmar.h (used lm.h instead), so failing
+#   to find levmar.h will result in LEVMAR_FOUND=FALSE.
+
+# how we identify ourselves
+set(_dep "LEVMAR")
+
+# Option to enable extra debug output from this module
+option(Teem_DEBUG_${_dep} "Print detailed debug about finding ${_dep}" OFF)
+
+# Helper macros to print only when Teem_DEBUG_${_dep} is enabled
+macro(_status msg)
+  if(Teem_DEBUG_${_dep})
+    message(STATUS "[Find${_dep}] ${msg}")
+  endif()
+endmacro()
+macro(_check_start msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_START "[Find${_dep}] ${msg}")
+  endif()
+endmacro()
+macro(_check_fail msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_FAIL "${msg}")
+  endif()
+endmacro()
+macro(_check_pass msg)
+  if(Teem_DEBUG_${_dep})
+    message(CHECK_PASS "${msg}")
+  endif()
+endmacro()
+
+# Try to find directory containing levmar.h
+# https://cmake.org/cmake/help/latest/command/find_path.html
+find_path(LEVMAR_INCLUDE_DIR
+  NAMES levmar.h
+  HINTS
+    $ENV{LEVMAR_DIR}
+    ${CMAKE_INSTALL_PREFIX}
+    /usr
+    /usr/local
+  PATH_SUFFIXES
+    include
+    include/levmar
+  DOC "Directory containing levmar.h"
 )
+_status("find_path result: LEVMAR_INCLUDE_DIR='${LEVMAR_INCLUDE_DIR}'")
 
-find_library(LEVMAR_LIBRARY levmar
-  /usr/lib
-  /usr/local/lib
-)
+# Learn the levmar version number from levmar.h. Versions 2.4 and prior didn't have a
+# levmar.h; they had lm.h, so failing to find levmar.h is one brute-force way to enforce
+# that we're looking at a version >= 2.5.  Version 2.5 has:
+#     #define LM_VERSION "2.5 (December 2009)"
+# and Version 2.6 has:
+#     #define LM_VERSION "2.6 (November 2011)"
+# To learn the version from levmar.h:
+if(LEVMAR_INCLUDE_DIR AND EXISTS "${LEVMAR_INCLUDE_DIR}/levmar.h")
+  file(READ "${LEVMAR_INCLUDE_DIR}/levmar.h" _levmar_header)
 
-set(LEVMAR_FOUND FALSE)
-if(LEVMAR_INCLUDE_DIR AND LEVMAR_LIBRARY)
-    set(LEVMAR_INCLUDE_DIRS ${LEVMAR_INCLUDE_DIR} )
-    set(LEVMAR_LIBRARIES ${LEVMAR_LIBRARY} )
-    set(LEVMAR_FOUND TRUE)
+  # Match e.g.: #define LM_VERSION "2.6 (November 2011)"
+  # https://cmake.org/cmake/help/latest/command/string.html
+  # sets CMAKE_MATCH_<N> for the N'th parethesized subexpression
+  string(REGEX MATCH "#define[ \t]+LM_VERSION[ \t]+\"([0-9]+\\.[0-9]+)" _match "${_levmar_header}")
+
+  if(CMAKE_MATCH_1)
+    set(LEVMAR_VERSION "${CMAKE_MATCH_1}")
+    _status("From levmar.h parsed version: ${LEVMAR_VERSION}")
+  endif()
+  unset(_levmar_header)
 endif()
 
-mark_as_advanced(
-  LEVMAR_INCLUDE_DIR
-  LEVMAR_LIBRARIES
-  LEVMAR_FOUND
-  )
+# Try to find full path to liblevmar library file
+find_library(LEVMAR_LIBRARY
+  NAMES levmar
+  HINTS
+    $ENV{LEVMAR_DIR}
+    ${CMAKE_INSTALL_PREFIX}
+    /usr
+    /usr/local
+  PATH_SUFFIXES
+    lib
+    lib64
+  DOC "Full path to the liblevmar.\{a\|so\|dylib\|lib\} file"
+)
+_status("find_library result: LEVMAR_LIBRARY='${LEVMAR_LIBRARY}'")
+
+# If we found the library but levmar *may* depend in turn on LAPACK and BLAS (more levmar
+# functions are available that way), so we try to find them using their built-in CMake
+# modules.  It's not that CMake can detect now whether the levmar found indeed depends on
+# LAPACK and BLAS; we just try to find them if we did find levmar.  And we don't if
+# LAPACK or BLAS are not found, but if they are required for this levmar, then we may
+# have linker error later.
+set(_dep_libraries "")  # list of transitive dependencies
+if(LEVMAR_LIBRARY)
+  _status("Looking for LAPACK & BLAS (may trigger Threads find)")
+  # if we didn't find LEVMAR, no point in looking for LAPACK, BLAS
+  find_package(LAPACK QUIET)
+  if(LAPACK_FOUND)
+    _status("LAPACK found: ${LAPACK_LIBRARIES}")
+    list(APPEND _dep_libraries ${LAPACK_LIBRARIES})
+  else()
+    _status("LAPACK *not* found")
+  endif()
+
+  find_package(BLAS QUIET)
+  if(BLAS_FOUND)
+    _status("BLAS found: ${BLAS_LIBRARIES}")
+    list(APPEND _dep_libraries ${BLAS_LIBRARIES})
+  else()
+    _status("BLAS *not* found")
+  endif()
+endif()
+
+# Deduplicate only transitive dependencies (LAPACK/BLAS) to avoid ld warnings
+list(REMOVE_DUPLICATES _dep_libraries)
+
+# Include the helper macro for standard handling of results
+include(FindPackageHandleStandardArgs)
+_check_start("find_package_handle_standard_args(LEVMAR)")
+# Standard handling macro: sets LEVMAR_FOUND if successful
+find_package_handle_standard_args(LEVMAR
+  REQUIRED_VARS LEVMAR_INCLUDE_DIR LEVMAR_LIBRARY
+  # report to caller of find_package(LEVMAR) what version they're getting
+  VERSION_VAR LEVMAR_VERSION
+)
+
+if(LEVMAR_FOUND)
+  set(LEVMAR_INCLUDE_DIRS ${LEVMAR_INCLUDE_DIR})
+
+  # for explicit linking by old-style code
+  set(LEVMAR_LIBRARIES ${_dep_libraries})
+  list(APPEND LEVMAR_LIBRARIES ${LEVMAR_LIBRARY})
+
+  # If we did find LEVMAR, but no imported target exists yet,
+  # create our own IMPORTED target so downstream code can use:
+  #     target_link_libraries(myprog PRIVATE LEVMAR::LEVMAR)
+  if(NOT TARGET LEVMAR::LEVMAR)
+    add_library(LEVMAR::LEVMAR UNKNOWN IMPORTED)
+    set_target_properties(LEVMAR::LEVMAR PROPERTIES
+      IMPORTED_LOCATION             "${LEVMAR_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${LEVMAR_INCLUDE_DIR}"
+      INTERFACE_LINK_LIBRARIES      "${_dep_libraries}" # only LAPACK/BLAS, NOT LEVMAR_LIBRARY itself
+    )
+  endif()
+  _check_pass("Found:\n      ${_dep}_INCLUDE_DIR=${${_dep}_INCLUDE_DIR}\n      ${_dep}_LIBRARY=${${_dep}_LIBRARY}")
+else()
+  _check_fail("Not found")
+endif()
+
+# clean up
+unset(_dep)
+unset(_status)
+unset(_check_start)
+unset(_check_fail)
+unset(_check_pass)
